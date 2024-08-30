29d75935fc10b73bbe73c13b2d9ed59f01fca49c
aminhavo <117644068+aminhavo@users.noreply.github.com>
2024-08-30 14:46:17 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,115 @@
+export const perguntas = [
+    {
+        enunciado: "1. Você prefere qual das opções a seguir para fazer um churraz na lage com sua família?",
+        alternativas: [
+            {
+                texto: "A) Chamas do Ben 10",
+                afirmacao: [
+                    " Ele será sua churrasqueira com combustivel infinito fazendo você economizar."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B) Antônio Bezerra",
+                afirmacao: [
+                    " Ele vai ser contra o churraz e vai escrever um livro te diminuindo como ser humano."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "2. Você prefere qual das opções a seguir para fazer uma festa surpreza o fazendo feliz?",
+        alternativas: [
+            {
+                texto: "Massa cinzenta de Ben 10",
+                afirmacao: [
+                    " Ele ira te agredir e chingar(ele já disse que não queria festa).",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: " Barack Obama",
+                afirmacao: [
+                    " Ele ira te dar 1 milhão de dolares."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: " Você prefere qual das opções a seguir para dançar",
+        alternativas: [
+            {
+                texto: "Michael jackson",
+                afirmacao: [
+                    "Ele ira te ajudar a dançar te encinando e te tornara seu filho."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Roberto Carlos",
+                afirmacao: [
+                    "Ele ira te mandar calar a sua boca e te espancar."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: " Você prefere qual das opções a seguir para dar grau?",
+        alternativas: [
+            {
+                texto: " Jesus",
+                afirmacao: [
+                    "Ele ira te encinar a dar grau no céu."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "ATUMALAKA",
+                afirmacao: [
+                    " Ele ira rir da sua cara se você cair(mesmo morto)."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Você prefere qual das opções a seguir para fugir da policia?",
+        alternativas: [
+            {
+                texto: "Pedrinho matador de porco",
+                afirmacao: [
+                    " Ele vai te falar: é vapo vapo nelas que gostão de ladrão, e deixar a policia prender vc e ele."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Rodorfo",
+                afirmacao: [
+                    " Ele vai te deixar subir nele e assim vão tirar a policia da cola"
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Você prefere qual das opções a seguir para confiar seus maiores segredos, e sempre estara para ele, sempre que ele precisar você estara para ele, não ter nenhuma disconfiança e nenhuma duvida de que ele te retribuira da mesma forma já que são melhores amigos, mais brothers?",
+        alternativas: [
+            {
+                texto: " Jhonatan",
+                afirmacao: [
+                    " ..."
+                ],
+            },
+            {
+                texto: "Mateus",
+                afirmacao: [
+                    "Ele ira fazer tudo isso..."
+                ],
+            },
+        ]
+    },
+];
